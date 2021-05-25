6bee6ad87930c485e1eab04f4c3221626f43ff80
Tomás Peiretti <73491345+elPeiretti@users.noreply.github.com>
2021-05-25 14:02:31 -0300

    Update snake.js

--- snake-game/snake.js
+++ snake-game/snake.js
@@ -1,113 +1,71 @@
-var canvas, context;
+import "./SnakeGame.js";
 
-window.onload = function(){
-    canvas = document.getElementById("canvas")
-    context = canvas.getContext("2d")
-    document.addEventListener("keydown", keyDownEvent)
-
-    //render speed
-    setInterval(draw,1000/15)
-}
-
-var width = 400;
-var height = 400;
-var prev=-1;
-var tail = 0;
-var snake = [];
-var posx = width/2;
-var posy = height/2;
-var var_posx=0;
-var var_posy=0;
-var foodx= Math.floor(Math.random()*(width-10)/10)*10;
-var foody= Math.floor(Math.random()*(height-10)/10)*10;
-
-function keyDownEvent(event){
-    
+function keyDownEvent(event, game){
+        
     switch(event.keyCode){
         //left
         case 37:
-            if (prev!=39){
-                var_posx=-10;
-                var_posy=0;
-                prev=37;
+            if (game.prev!=39){
+                game.var_posx=-10;
+                game.var_posy=0;
+                game.prev=37;
             }
             break;
         //up
         case 38:
-            if (prev!=40){
-                var_posx=0;
-                var_posy=-10;
-                prev=38;
+            if (game.prev!=40){
+                game.var_posx=0;
+                game.var_posy=-10;
+                game.prev=38;
             }
             break;
         //right
         case 39:
-            if (prev!=37){
-                var_posx=10;
-                var_posy=0;
-                prev=39;
+            if (game.prev!=37){
+                game.var_posx=10;
+                game.var_posy=0;
+                game.prev=39;
             }
             break;
         //down
         case 40:
-            if (prev!=38){
-                var_posx=0;
-                var_posy=10;
-                prev=40;
+            if (game.prev!=38){
+                game.var_posx=0;
+                game.var_posy=10;
+                game.prev=40;
             }
             break;
-
+        //enter
+        case 13:
+            if(game.GAMEOVER){
+                game.restart();
+            }
     }
-}
-
-function between(x,a,b){
-    return x>=a && x<=b;
-}
-
-function validatePosition(x,y) {
-    return between(x,0,width) && between(y,0,height) && undefined===snake.find(elem => elem[0]==x && elem[1]==y);
 }
 
 function draw(){
-    posx+=var_posx;
-    posy+=var_posy;
-    if(validatePosition(posx,posy)){
-        if(posx==foodx && posy==foody){
-            foodx= Math.floor(Math.random()*(width-10)/10)*10
-            foody= Math.floor(Math.random()*(height-10)/10)*10
-            snake.push([posx,posy]);
-        }
-        if(snake.length>1){
-            var i=0;
-            while(i<snake.length){
-                snake[i]=snake[i+1];
-                i++;
-            }
-            snake[snake.length-1] = [posx-var_posx,posy-var_posy];
-        }
-        else if(snake.length==1){
-            snake[snake.length-1] = [posx-var_posx,posy-var_posy];
-        }
+
+    g.gameLoop();
+    if(g.GAMEOVER){
+        g.gameOver();
     }
     else{
-        //TODO
-        console.log("OUT OF RANGE")
+        g.paintCanvas();
     }
-
-    //color bg
-    context.fillStyle = "gray";
-    context.fillRect(0,0,width,height);
-
-    //color snake
-    context.fillStyle="yellow";
-    context.fillRect(posx,posy, 10,10);
-    var i=0;
-    while(i<snake.length){
-        context.fillRect(snake[i][0], snake[i][1], 10, 10);
-        i++;
-    }
-    //color food
-    context.fillStyle = "red";
-    context.fillRect(foodx, foody, 10,10);
+    
 }
+
+
+var canvas, context;
+var g;
+
+window.onload = function(){
+    canvas = document.getElementById("canvas")
+    context = canvas.getContext("2d")
+    g = new SnakeGame(canvas,context);
+    document.addEventListener("keydown", (ev) => (keyDownEvent(ev,g)));
+    //render speed
+    setInterval(draw,1000/15)
+}
+
 
